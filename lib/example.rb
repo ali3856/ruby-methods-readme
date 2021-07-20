f04e046e453c39e9b07df747892a40ce6691c2c5
Asim Ali <asimsali95@gmail.com>
2021-07-19 19:28:33 -0500

    read and solved

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,29 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def say_hello_ten_times
+    phrase = "Hello World!"
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+end
+
+say_hello_ten_times
+
+#alternate solution below with loop
+def greeting 
+    puts "Hello World!"
+end
+
+count = 0
+
+until count == 10
+    greeting
+    count += 1
+end
